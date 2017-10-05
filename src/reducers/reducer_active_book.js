6b5b9df5838c48fc4dbcf3c52631cf89fb3f76e4
Remus Daniel Heghedus <hrdaniel4877@gmail.com>
2017-10-05 17:26:58 -0400

    Create BookDetail container

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,7 +1,7 @@
 // 'state' argument is NOT app state;
 // only the state this reducer is responsible for
 export default function (state=null, action) {
-	switch (sction.type) {
+	switch (action.type) {
 		case 'BOOK_SELECTED':
 			return action.payload;
 	};
